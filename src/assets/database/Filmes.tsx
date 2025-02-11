2705c4870f06bc1e4f53d42ab875f33ba35e9564
Lucas Gomes <lukashitoh@gmail.com>
2025-02-11 17:54:24 -0200

    add: info de filmes

---
+++ src/assets/database/Filmes.tsx
@@ -0,0 +1,31 @@
+
+const Filmes ={
+        "Filmes": [
+            {
+                "id": 1,
+                "titulo": "O Poderoso Chefão",
+                "ano": 1972,
+                "diretor": "Francis Ford Coppola",
+                "poster": "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/3bhkrj58Vtu7enYsRolD1fZdja1.jpg",
+                "genero": "Crime"
+            },
+            {
+                "id": 2,
+                "titulo": "O Senhor dos Anéis: A Sociedade do Anel",
+                "ano": 2001,
+                "diretor": "Peter Jackson",
+                "poster": "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/56zTpe2xvaA4alU51sRWPoKPYZy.jpg",
+                "genero": "Aventura"
+            },
+            {
+                "id": 3,
+                "titulo": "O Senhor dos Anéis: As Duas Torres",
+                "ano": 2002,
+                "diretor": "Peter Jackson",
+                "poster": "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/5VTN0pR8gcqV3EPUHHfMGnJYN9L.jpg",
+                "genero": "Aventura"
+            }
+        ]
+}
+
+export default Filmes
