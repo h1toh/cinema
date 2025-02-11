f136fb7b19a758805a7d0fab709bd8efaf07b009
Lucas Gomes <lukashitoh@gmail.com>
2025-02-11 19:58:40 -0200

    corrigindo home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,12 +1,12 @@
 import Filmes from "../assets/database/Filmes"
 
 const Home = () => {
-    return <div>
-        <h1 className="hometitle">Em alta</h1>
+    return <div className="home">
+        <h1 className="hometitle">Filmes em alta</h1>
         <div className="filmes">
             {Filmes.Filmes.map((filme) => (
                 <div key={filme.id} className="filmeCartaz">
-                    <img src={filme.poster} alt={filme.titulo} />
+                    <img className="poster" src={filme.poster} alt={filme.titulo} />
                     <h2>{filme.titulo}</h2>
                     <p>{filme.ano} - {filme.diretor}</p>
                     <p><strong>Genero</strong> {filme.genero}</p>
