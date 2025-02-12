2d5440cfe52f9dae87d492964ea2c189a0d6666c
Lucas Gomes <lukashitoh@gmail.com>
2025-02-12 00:07:04 -0200

    atualizando Main

---
+++ src/components/Main.tsx
@@ -0,0 +1,22 @@
+import Filmes from "../assets/database/Filmes"
+import { Link } from "react-router-dom"
+
+const Main = () => {
+    return <div className="home">
+        <h1 className="hometitle">Filmes em alta</h1>
+        <div className="filmes">
+            {Filmes.Filmes.map((filme) => (
+                <Link to={"/filme/" + filme.route}>   
+                <div key={filme.id} className="filmeCartaz">
+                    <img className="poster" src={filme.poster} alt={filme.titulo} />
+                    <h2>{filme.titulo}</h2>
+                    <p>{filme.ano} - {filme.diretor}</p>
+                    <p><strong>Genero</strong> {filme.genero}</p>
+                </div>
+                </Link>
+            ))}
+        </div>
+    </div>
+}
+
+export default Main
