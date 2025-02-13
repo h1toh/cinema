8c53628ebbec57cac48e95bf34ec0976161b240d
Lucas Gomes <lukashitoh@gmail.com>
2025-02-13 15:31:29 -0200

    ajustando info

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom"
 import Filmes from "../assets/database/Filmes"
 import ScrollButton from "./ScrollButton";
+import YoutubeTrailer from "./YoutubeTrailer";
 
 const Info = () => {
 
@@ -23,15 +24,15 @@ const Info = () => {
                     <span><h1>{filme.titulo}</h1> <p>({filme.ano})</p></span>
                     <p>{filme.duracao} | {filme.genero} {filme.classificacao}</p>
                     <p>{filme.sinopse}</p>
+
                     <div className="buttons">
-                        <ScrollButton valor="Ver sessoes" nomeClasse="btnSessoes" id="sessoes"/>
-                        <ScrollButton valor="Ver trailer" nomeClasse="btnTrailer" id="trailer"/>
+                        <ScrollButton valor="Ver sessoes" nomeClasse="btnSessoes" id="sessoes" />
+                        <ScrollButton valor="Assistir trailer" nomeClasse="btnTrailer" id="trailer" />
                     </div>
                 </div>
             </div>
-            <div className="trailer" id="trailer">
-                <iframe width="60%" height="500" src={filme.trailerYoutubeEmbed} title={filme.titulo} frameBorder="0"></iframe>
-            </div>
+            
+            <YoutubeTrailer title={filme.titulo} src={filme.trailerYoutubeEmbed} />
         </>
     )
 }
