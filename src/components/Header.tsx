1802de60239f4aaae873404da928a9008c23e027
Lucas Gomes <lukashitoh@gmail.com>
2025-02-13 02:37:31 -0200

    colocando favicon e meu github no header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,10 +2,13 @@ import { Link } from "react-router-dom"
 
 const Header = () => {
     return <div className="header">
-        <Link to="/">
+        <Link className="headerLink" to="/">
             <h1 className="headertitle">Cinema</h1>
+            <img className="favicon" src="../public/favicon.png" alt="" />
+        </Link>
+        <Link className="githubLink" to="https://github.com/h1toh">
+            <img className="githubImg" src="../src/assets/images/github/github-mark-white.png" alt="" />
         </Link>
     </div>
 }
-
 export default Header
