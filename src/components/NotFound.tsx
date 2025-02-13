4115f8314ba058252238dfa87d212b22e3d0616f
Lucas Gomes <lukashitoh@gmail.com>
2025-02-13 16:58:33 -0200

    add filme nao encontrado

---
+++ src/components/NotFound.tsx
@@ -0,0 +1,10 @@
+const NotFound = () => {
+    return (
+        <div className="notFound">
+            <img src="../src/assets/images/popcorn.png" alt="dropped popcorn" />
+            <h3>Parece que o filme não foi encontrado.</h3>
+        </div>
+    )
+}
+
+export default NotFound
