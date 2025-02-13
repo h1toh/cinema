c5f61b9ff5eb3238786420185ecabf4932db4884
Lucas Gomes <lukashitoh@gmail.com>
2025-02-13 16:58:46 -0200

    atualizando header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,11 +3,10 @@ import { Link } from "react-router-dom"
 const Header = () => {
     return <div className="header">
         <Link className="headerLink" to="/">
-            <h1 className="headertitle">Cinema</h1>
-            <img className="favicon" src="../public/favicon.png" alt="" />
+            <h1 className="headertitle">Cinema.com</h1>
         </Link>
         <Link className="githubLink" to="https://github.com/h1toh">
-            <img className="githubImg" src="../src/assets/images/github/github-mark-white.png" alt="" />
+            <img className="githubImg" src="../src/assets/images/github/github-mark-white.svg" title="Github" alt="Github" />
         </Link>
     </div>
 }
