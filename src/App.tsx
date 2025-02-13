e76e2c4b6ed44e97afa6a4cb9d5e1a5c647995b7
Lucas Gomes <lukashitoh@gmail.com>
2025-02-13 14:46:12 -0200

    pequena alteracao no App Route

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import Header from "./components/Header"
 import Home from "./pages/Home"
 import Assentos from "./pages/Assentos"
 import Ingressos from "./pages/Ingressos"
-import Sessoes from "./pages/Sessoes"
 import Pagamento from "./pages/Pagamento"
 import Filme from "./pages/Filme"
 
@@ -18,10 +17,10 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/filme/:route" element={<Filme />} />
-          <Route path="/sessoes/:filme" element={<Sessoes />} />
           <Route path="/assentos/:sessao" element={<Assentos />} />
           <Route path="/ingressos/:sessao" element={<Ingressos />} />
           <Route path="/pagamento/:sessao" element={<Pagamento />} />
+          <Route path="*" element={<Home/>} />
         </Routes>
 
       </BrowserRouter>
