1092e4fe8e9911f9a4df116d7b8d3c602f350fef
Lucas Gomes <lukashitoh@gmail.com>
2025-02-13 02:35:47 -0200

    footer removido

--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import { Link } from "react-router-dom";
-
-const Footer = ()=>{
-    return <div className="footer">
-        <Link to="https://github.com/h1toh" target="_blank">Desenvolvido por Lucas Gomes</Link>
-    </div>
-}
-
-export default Footer; 
