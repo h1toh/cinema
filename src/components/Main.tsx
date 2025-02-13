206ba48868878cc0ce58792996c51f66ecd60998
Lucas Gomes <lukashitoh@gmail.com>
2025-02-13 15:31:53 -0200

    new commit

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -6,13 +6,13 @@ const Main = () => {
         <h1 className="hometitle">Filmes em Cartaz</h1>
         <div className="filmes">
             {Filmes.Filmes.map((filme) => (
-                <Link to={"/filme/" + filme.route}>   
-                <div key={filme.id} className="filmeCartaz">
-                    <img className="poster" src={filme.poster} alt={filme.titulo} />
-                    <h2>{filme.titulo}</h2>
-                    <p>{filme.ano} - {filme.diretor}</p>
-                    <p><strong>Genero</strong> {filme.genero}</p>
-                </div>
+                <Link to={"/filme/" + filme.route}>
+                    <div key={filme.id} className="filmeCartaz">
+                        <img className="poster" src={filme.poster} alt={filme.titulo} />
+                        <h2>{filme.titulo}</h2>
+                        <p>{filme.ano} - {filme.diretor}</p>
+                        <p><strong>Genero</strong> {filme.genero}</p>
+                    </div>
                 </Link>
             ))}
         </div>
