01512542039bf2cd72d70af6e2ddd827cc46e263
Lucas Gomes <lukashitoh@gmail.com>
2025-02-28 01:14:27 -0300

    teste

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,9 +6,10 @@ const port: number = 8080;
 
 app.use(cors({ origin: "http://localhost:3000" }))
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello, world!');
+app.get('/api', (req: Request, res: Response) => {
+    res.send('hello')
 })
+
 
 app.listen(port, () => {
     console.log(`Server rodando em http://localhost:${port}`);
