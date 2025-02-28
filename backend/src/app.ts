477faba2dfaed4006b398d9c55098fc79767f1d6
Lucas Gomes <lukashitoh@gmail.com>
2025-02-27 22:55:36 -0300

    add cors no backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,10 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response } from "express"
+import cors from "cors"
 
 const app = express();
 const port: number = 8080;
+
+app.use(cors({ origin: "http://localhost:3000" }))
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello, world!');
